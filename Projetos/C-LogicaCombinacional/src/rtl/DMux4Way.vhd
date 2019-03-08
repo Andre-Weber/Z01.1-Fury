d54fc12de55e35af0e62e3ba4a1507e4df8a17b4
Vitor Grando Eller <vitorge@al.insper.edu.br>
2019-03-08 08:01:28 -0300

    Corrected Dmux4Way

--- Projetos/C-LogicaCombinacional/src/rtl/DMux4Way.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/DMux4Way.vhd
@@ -14,19 +14,9 @@ end entity;
 architecture rtl of DMux4Way is
 begin
 
-
-	process (a, sel)is
-	begin
-		if (sel = "00") then
-		q0 <= a;
-		elsif (sel = "10") then
-		q1 <= a;
-		elsif (sel = "01") then
-		q2 <= a;
-		else
-		q3 <= a;
-		end if;
-	end process;
-
+	q0 <= a when sel = "00" else '0';
+	q1 <= a when sel = "01" else '0';
+	q2 <= a when sel = "10" else '0';
+	q3 <= a when sel = "11" else '0';
 
 end architecture;
