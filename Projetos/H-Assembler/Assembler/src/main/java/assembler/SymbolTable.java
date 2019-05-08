740533b4bb1db80e656441844ac84210383de5c5
Pedro Paulo <pedropauloficial@hotmail.com>
2019-05-08 14:04:41 -0300

    symbol table funcionando

--- Projetos/H-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
+++ Projetos/H-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
@@ -26,38 +26,69 @@ public class SymbolTable {
 
     /**
      * Insere uma entrada de um símbolo com seu endereço numérico na tabela de símbolos.
-     * @param  symbol símbolo a ser armazenado na tabela de símbolos.
-     * @param  address símbolo a ser armazenado na tabela de símbolos.
+     *
+     * @param symbol  símbolo a ser armazenado na tabela de símbolos.
+     * @param address símbolo a ser armazenado na tabela de símbolos.
      */
     public void addEntry(String symbol, int address) {
-
+        symbolTable.put(symbol, address);
     }
 
     /**
      * Confere se o símbolo informado já foi inserido na tabela de símbolos.
-     * @param  symbol símbolo a ser procurado na tabela de símbolos.
+     *
+     * @param symbol símbolo a ser procurado na tabela de símbolos.
      * @return Verdadeiro se símbolo está na tabela de símbolos, Falso se não está na tabela de símbolos.
      */
-    public Boolean contains(String symbol){
-    	return false;
+    public Boolean contains(String symbol) {
+        if(symbolTable.containsKey(symbol)){
+            return true;
+        }
+        return false;
     }
 
     /**
      * Retorna o valor númerico associado a um símbolo já inserido na tabela de símbolos.
-     * @param  symbol símbolo a ser procurado na tabela de símbolos.
+     *
+     * @param symbol símbolo a ser procurado na tabela de símbolos.
      * @return valor numérico associado ao símbolo procurado.
      */
     public Integer getAddress(String symbol) {
-    	return null;
+        if(symbolTable.containsKey(symbol)){
+            return symbolTable.get(symbol);
+        }
+        return null;
     }
-
-    /**
-    * Cria os simbolos padroes na tabela
-    * ref:
-    */
-    public void initialize() {
-        this.addEntry("R0", 0);
-
-    }
+        /**
+         * Cria os simbolos padroes na tabela
+         * ref:
+         */
+        public void initialize () {
+            this.addEntry("R0", 0);
+            this.addEntry("R1", 1);
+            this.addEntry("R2", 2);
+            this.addEntry("R3", 3);
+            this.addEntry("R4", 4);
+            this.addEntry("R5", 5);
+            this.addEntry("R6", 6);
+            this.addEntry("R7", 7);
+            this.addEntry("R8", 8);
+            this.addEntry("R9", 9);
+            this.addEntry("R10", 10);
+            this.addEntry("R11", 11);
+            this.addEntry("R12", 12);
+            this.addEntry("R13", 13);
+            this.addEntry("R14", 14);
+            this.addEntry("R15", 15);
+            this.addEntry("SP", 0);
+            this.addEntry("LCL", 1);
+            this.addEntry("ARG", 2);
+            this.addEntry("THIS", 3);
+            this.addEntry("THAT", 4);
+            this.addEntry("SCREEN", 16384);
+            this.addEntry("LED", 21184);
+            this.addEntry("SW", 21185);
+        }
 
 }
+
